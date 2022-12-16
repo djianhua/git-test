5e2112f6b65f1af24c0f4c4f6dc9a5b281676cf4
huagege <3311646194@qq.com>
2022-12-16 09:38:53 +0800

    second commit

--- src/main/java/com/hua/Main.java
+++ src/main/java/com/hua/Main.java
@@ -3,5 +3,6 @@ package com.hua;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world2!");
     }
 }
