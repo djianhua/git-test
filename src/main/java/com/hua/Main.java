443960847c478720cc4a4927d7654f4d32f5aff8
hua <3311646194@qq.com>
2022-12-16 15:21:07 +0800

    gitee commit

--- src/main/java/com/hua/Main.java
+++ src/main/java/com/hua/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello world2!");
         System.out.println("Hello world!");
         System.out.println("push commit");
+        System.out.println("gitee test");
     }
 }
